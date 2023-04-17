2ef406bffcadb45fc4d9be77e6e52b3f27b0129c
namydhc1 <92814689+namydhc1@users.noreply.github.com>
2023-04-17 20:52:24 +0700

    create DBcontext

---
+++ Namqdhe153206_prj/src/java/dal/DBContext.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "sa";
+            String pass = "admin1234@";
+            String url = "jdbc:sqlserver://LAPTOP-8DH1K3EM\\SQLEXPRESS:1433;databaseName=PRJ301_B5_FA22";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
